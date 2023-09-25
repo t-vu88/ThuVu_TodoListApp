1da1dce6a8b994d79d4b0204f07daaaf5d06e57d
Thu Trang Vu <vuthutrang2811@gmail.com>
2023-09-25 10:26:21 +0300

    tehtävä valmis

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [todo, setTodo] = useState({desc:"", date: ""});
+  const [todos,setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({...todo,[event.target.name]: event.target.value});
+  };
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  };
+
+  return (
+    <div className = "App">
+      <div className = "addTodoBox">
+        <h1>Add todo:</h1>
+        <form onSubmit = {addTodo}>
+        <label>Description:</label>
+          <input type="text" name = "desc" onChange={inputChanged} value={todo.desc}></input>
+          <label>Date:</label>
+          <input type ="date" name = "date" onChange={inputChanged} value ={todo.date}></input>
+          <input type ="submit" value ="Add"></input>  
+          </form>
+      </div>
+        <table>
+
+        <tbody>
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+          </tr>
+          
+          {todos.map((todo, index) => (        
+          <tr key={index}>
+            <td>{new Date(todo.date).toLocaleDateString('fi-FI')}</td>
+            <td>{todo.desc}</td>
+          </tr>
+          ))}
+        </tbody>
+        
+      </table>
+    </div>
+  )
+}
+
+export default App
